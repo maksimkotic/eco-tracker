f6940148be03e50dfd54c5c9a76802860aaf634a
maksimkotic <mkutov13@gmail.com>
2026-04-23 23:51:36 +0500

    Починка смены пароля

--- models/User.js
+++ models/User.js
@@ -27,6 +27,13 @@ module.exports = (sequelize, DataTypes) => {
           notEmpty: true,
         },
       },
+      password: {
+        type: DataTypes.VIRTUAL,
+        allowNull: true,
+        validate: {
+          len: [6, 255],
+        },
+      },
       passwordHash: {
         type: DataTypes.STRING,
         allowNull: false,
@@ -81,7 +88,7 @@ module.exports = (sequelize, DataTypes) => {
           }
         },
         beforeUpdate: async (user) => {
-          if (user.changed("password")) {
+          if (user.changed("password") && user.password) {
             user.passwordHash = await bcrypt.hash(user.password, 10);
           }
         },
